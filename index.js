2e0708e068c144177b949ef511a4f5bd48a3bd60
Test2000242 <141023710+Test2000242@users.noreply.github.com>
2024-05-03 20:26:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 8005;
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Define a route with query parameters
+app.post('/social/callback/success', (req, res) => {
+        console.log(req.body);
+        res.send(`<script>window.opener.postMessage(${JSON.stringify(req.body)}, '*');</script>`);
+    });
+
+    
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
