cc188028c679e08bf0a0747c027b6cceb6f5d4a8
Test2000242 <141023710+Test2000242@users.noreply.github.com>
2024-05-04 16:18:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8005;
 
-app.use(express.json())
+//app.use(express.json())
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
